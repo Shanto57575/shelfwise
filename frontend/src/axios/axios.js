e9bd754168dd0a0812cb8518bd04daac55b7d8d0
Shanto57575 <shanto57575@gmail.com>
2024-12-23 22:54:49 +0600

    deploy issues solved

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = axios.create({
-    baseURL: 'https://shelfwise-backend.vercel.app',
+    baseURL: 'https://shelfwisebackend.vercel.app',
 });
 
 export default API_BASE_URL;
